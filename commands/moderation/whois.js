71382a8613a70d34e388f67ee323f9cbccc13391
BulbagardenTech <technical@bulbagarden.net>
2023-10-16 16:16:28 -0400

    Whois now accepts User ID

--- commands/moderation/whois.js
+++ commands/moderation/whois.js
@@ -11,10 +11,23 @@ module.exports = {
         .setDescription('Displays information about a user account.')
         .addUserOption(user =>
             user.setName('user')
-                .setDescription('User to inspect.')
-                .setRequired(true)),
+                .setDescription('User to inspect.'))
+        .addStringOption(userID =>
+            userID.setName("userid")
+                .setDescription("User ID (if user cannot be found).")),
     async execute(interaction) {
-        const user = interaction.options.getUser("user");
+        let user = interaction.options.getUser("user");
+        if (!user) {
+            const userID = interaction.options.getString("userid");
+            if (!userID)
+                return interaction.reply("You must supply either a user or their ID.");
+            user = await interaction.client.users.fetch(userID).catch(err => {
+                console.log(err);
+                return interaction.reply("User not found.");
+            });
+            if (!user)
+                return interaction.reply(`No user found with ID ${userID}.`);
+        }
         const modRole = await interaction.guild.roles.fetch(modID);
         if (interaction.member.roles.highest.position < modRole.position) {
             interaction.client.emit('unauthorized', interaction.client, interaction.user, {
@@ -23,42 +36,55 @@ module.exports = {
             });
             return interaction.reply("You are not authorized to perform this command. Repeated attempts to perform unauthorized actions may result in a ban.");
         }
-        const member = await interaction.guild.members.fetch(user);
+        const member = await interaction.guild.members.fetch(user).catch(err => {
+            console.log(err);
+        });
+
         let status = [];
         let game = [];
-        const customStatus = member.presence.activities.find(act => act.name === "Custom Status");
-        const playing = member.presence.activities.find(act => act.name === "Playing");
-        if (customStatus && playing) {
-            status.push(customStatus.state);
-            game.push(playing.name);
-        } else if (playing && !customStatus) {
-            status.push("Playing");
-            game.push(playing.name);
-        } else if (customStatus && !playing) {
-            status.push(customStatus.state);
-        } else status.push(member.presence.status);
-        if (!status.length)
-            status.push("N/A");
-        if (!game?.length)
-            game.push("None");
+        if (member && member.presence && member.presence.activities) {
+            const customStatus = member.presence.activities.find(act => act.name === "Custom Status");
+            const playing = member.presence.activities.find(act => act.name === "Playing");
+            if (customStatus && playing) {
+                status.push(customStatus.state);
+                game.push(playing.name);
+            } else if (playing && !customStatus) {
+                status.push("Playing");
+                game.push(playing.name);
+            } else if (customStatus && !playing) {
+                status.push(customStatus.state);
+            } else status.push(member.presence.status);
+            if (!status.length)
+                status.push("N/A");
+            if (!game?.length)
+                game.push("None");
+        }
+        let roles = {};
+        if (!member)
+            roles = "Not in server";
+        else {
+            roles.name = "Roles";
+            roles.value = member.roles.cache.size > 1 ? member.roles.cache.map(role => {
+                if (role.name !== "@everyone") return role.name + ", ";
+            }).filter(role => {
+                return role !== null
+            }).join("").trim().slice(0, -1) : "None"
+        }
         const response = new EmbedBuilder()
             .setColor(messageColors.whois)
             .setTitle(`Whois information for ${user.username}`)
             .setThumbnail(user.displayAvatarURL())
             .addFields([
                 {name: "ID", value: user.id, inline: true},
-                {name: "Nickname", value: member.nickname ? member.nickname : "None", inline: true},
-                {name: "Status", value: status[0], inline: true},
-                {name: "Game", value: game[0], inline: true},
-                {name: "Joined", value: member.joinedAt.toString(), inline: true},
-                {name: "Registered", value: user.createdAt.toString(), inline: true},
                 {
-                    name: "Roles", value: member.roles.cache.size > 1 ? member.roles.cache.map(role => {
-                        if (role.name !== "@everyone") return role.name + ", ";
-                    }).filter(role => {
-                        return role !== null
-                    }).join("").trim().slice(0, -1) : "None", inline: true
-                }
+                    name: "Nickname",
+                    value: member && member.nickname ? member.nickname : "None set or not in server",
+                    inline: true
+                },
+                {name: "Status", value: status.length !== 0 ? status[0] : "None", inline: true},
+                {name: "Game", value: game.length !== 0 ? game[0] : "None", inline: true},
+                {name: "Joined", value: member ? member.joinedAt.toString() : "Not in server", inline: true},
+                {name: "Registered", value: user.createdAt.toString(), inline: true},
             ])
             .setTimestamp();
         return interaction.reply({embeds: [response]});
